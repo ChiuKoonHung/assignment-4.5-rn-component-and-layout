2d5d05edda0e125fa85c791fafcfd2e363dd1a6e
ChiuKoonHung <chiukoonhung1991@gmail.com>
2021-12-01 17:08:12 +0800

    completed

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState(null);
+  const [age, setAge] = useState(null);
+  const [checked, setChecked] = React.useState('first');
+
+  handleText = () => {
+    useState(setText);
+  }
+
+  handleAge = () => {
+    useState(setAge);
+  }
+
+  submit = () => {
+    alert(' My name is ' + text +'. I am ' + age + ' years old.');
+  }
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Your Name:</Text>
+      <TextInput style={styles.input} value={text} onChangeText={setText}></TextInput>
+      <Text>Your Age:</Text>
+      <TextInput style={styles.input} value={age} onChangeText={setAge}></TextInput>
+
+      <TouchableOpacity
+        style = {styles.submitButton}
+        onPress = {
+          () => submit(text, age)}>
+        <Text style = {styles.submitButtonText}> Submit </Text>
+      </TouchableOpacity>
     </View>
+
   );
 }
 
@@ -18,4 +45,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  input: {
+    width: 200,
+    height: 40,
+    margin: 12,
+    borderWidth: 3,
+    padding: 10
+  },
+  submitButton: {
+    backgroundColor: '#7a42f4',
+    padding: 10,
+    margin: 15,
+    height: 40,
+ },
+ submitButtonText:{
+    color: 'white'
+ }
 });
---
+++ README.md
@@ -0,0 +1,17 @@
+React Native - Component and Layout : Assignment
+Brief
+In this assignment, you are to create a simple form as shown below.
+
+
+
+Upon submit button pressed, it should show an alert.
+
+
+
+You may install external dependency from npmjs.com. You should be able to refer to documentation at reactnative.dev to complete this task.
+
+Submission Guidelines
+Cite any relevant sources consulted during your research
+Solve the problems using your own code
+Do not copy and paste solutions from the source material
+Submit your assignment to black board.
